a3f7a9bfd27fce96cdf467c489ccca4e29284166
Anh <nguye538@umn.edu>
2018-05-26 20:09:42 -0500

    Add in code to update progress and track guessed letters

--- HW3/assets/javascript/game.js
+++ HW3/assets/javascript/game.js
@@ -1,6 +1,11 @@
+$(document).ready(function() {
 
 //Global Variables
 var currentword = "";
+var guessedLetters = [];
+var Wins = 0;
+var Losses = 0;
+var remainingGuesses = 10;
 
 //Hangman object
 var HangmanWord = {
@@ -12,41 +17,91 @@ var HangmanWord = {
          
     GenerateCurrentWord: function(){
     function getRandomInt(max) 
-    {
-    return Math.floor(Math.random() * Math.floor(max));
+        {
+        return Math.floor(Math.random() * Math.floor(max));
+        }
+        var newword = this.WordsList[getRandomInt(this.WordsList.length)];       
+        return newword;
     }
-    word = this.WordsList[getRandomInt(this.WordsList.length)];
-    return word;
-    },
+};
 
-    DisplayDashes: function(str){
-       wordlength = str.length;
-       var tempstr = "";
+//Helper Methods*********
 
-       for(i = 0; i < wordlength; i++)
-       {
-         tempstr += " _ ";
-       }
-       document.getElementById("display-correct-letters").innerHTML = tempstr;
-    },
+//Create and return empty dashes array for the current word
+function CreateDashes(word)
+{
+    var dashesArray = [];
+    for(i = 0; i < word.length; i++)
+    {   
+        dashesArray[i] = "_";
+    }    
+    return dashesArray;
 }
 
-//Variables for stats
-var Wins = 0;
-var Losses = 0;
-var RemainingGuesses = 9;
+//Display
+function DisplayProgress(id, array)
+{
+    document.getElementById(id).innerHTML = array.join(" ");
+}
 
-//Array to keep track of Guessed Letters
-var GuessedLetters = [];
+//Check if user's guess is in the word
+function UpdateProgress(guess, word, tempArray, guessedLetters)
+{
+    var letterExist = false;
 
+    function CheckGuess()
+    {
+        //Iterate through the each letter in the word and see if the user's guess matches 
+        for(var i = 0; i < word.length; i++)
+        {
+            if(word[i] === guess)
+            {
+                tempArray[i] = guess;       
+                DisplayProgress("display-correct-letters", tempArray);
+                letterExist = true;              
+            }
+        }
+        return letterExist;
+    }
+
+    if(CheckGuess() === false)
+    {
+        remainingGuesses--;
+        document.getElementById("remaining-guess-count").innerHTML = remainingGuesses;
+        guessedLetters.push(guess); 
+        document.getElementById("guessed-letters").innerHTML =  guessedLetters.join(" ");     
+    }
+
+    console.log(guessedLetters);
+    
+      
+}
 
 //Listen to spacebar event to start
+console.log("Starting game");
+
 document.onkeyup = function(e){
     if(e.keyCode == 13)
     {
         //Generate new word
-        currentword = HangmanWord.GenerateCurrentWord();
-        HangmanWord.DisplayDashes(currentword);
-    }
-}
+        var currentword = HangmanWord.GenerateCurrentWord();
+        console.log(currentword);
+        var progressArray = CreateDashes(currentword);
+        console.log(progressArray);
+        DisplayProgress("display-correct-letters", progressArray);
 
+        //Wait for user guess
+        document.onkeyup = function(event) 
+        {
+          //Check if key entered is a letter  
+          if((event.keyCode >= 65) && (event.keyCode <= 90))
+          {
+          var userGuess = event.key;
+          UpdateProgress(userGuess.toLocaleLowerCase(), currentword, progressArray, guessedLetters);
+          }       
+        }  
+    }
+  
+
+}
+});
