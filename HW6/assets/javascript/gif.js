ddd6e47f2f200c39405b1a0cc378e96fe6886618
Anh <nguye538@umn.edu>
2018-06-15 21:05:05 -0500

    Added functionality for the display gif and still/animate on click

--- HW6/assets/javascript/gif.js
+++ HW6/assets/javascript/gif.js
@@ -4,7 +4,12 @@ $(document).ready(function(){
 topics = ["cat", "dog", "bird", "fish", "koala", "dolphin"];
 var count = 10;
 var newAnimal;
-var queryURL
+
+var baseURL = "https://api.giphy.com/v1/gifs/search?"
+var queryParam = "q=";
+var limitParam = "&limit=10"; //number of records to return (10)
+var apiKey = "&api_key=dc6zaTOxFJmzC";
+var queryURL = "";
 
 console.log("Starting");
 
@@ -39,10 +44,68 @@ console.log(newAnimal);
 renderButton();
 })
 
-//Click event for each animal button
-{
+//Click event for each animal button. Make an ajax call to the api
+$(".animal-button").on("click", function(){
 
+   var animal = $(this).attr("data-buttonName");
+   queryURL = baseURL + queryParam + animal + limitParam + apiKey;
+   console.log(queryURL);
+
+   $.ajax({
+       url: queryURL,
+       method: 'GET'
+   }).then(function(response){
+     
+    var result = response.data;
+    // console.log(result);
+    console.log(result.length);
+
+    for(i = 0; i < result.length; i++)
+    {
+        //Get the rating, url for still image and gif 
+        var rating = result[i].rating;
+        console.log(rating);
+        var imgSrcAnimate = result[i].images.fixed_height.url;
+        var imgSrcStill = result[i].images.fixed_height_still.url; 
+        // console.log(imgSrcAnimate);
+        // console.log(imgSrcStill);
+
+        //Create new <img> and <p> elements
+        var newImgDiv = $("<img>");
+        var newP = $("<p>");
+
+        //Add class and attributes to the image element
+        newImgDiv.addClass("gif");
+        newImgDiv.attr("src", imgSrcStill);
+        newImgDiv.attr("data-still", imgSrcStill);
+        newImgDiv.attr("data-animate", imgSrcAnimate);
+        newImgDiv.attr("data-state", "still");
+
+        //Append to html
+        newP.text("Rating: " + rating);
+        $("#gifs-display").append(newP);
+        $("#gifs-display").append(newImgDiv);
+    }
+   })
+
+})
+
+//Click event when user click on image
+$("body").on("click", ".gif", function(){
+
+console.log("CLICKED IMAGE");
+//Get the data-state attribute of the image
+var state = $(this).attr("data-state");
+console.log(state);
+
+if (state === "still"){
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("data-state", "animate");
+}
+else{
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("data-state", "still")
 }
 
-
+})
 });
