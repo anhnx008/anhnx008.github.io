09d6ba44bedf5b2b397f9c39fbab3dcc6c44be60
Anh <nguye538@umn.edu>
2018-06-17 13:07:16 -0500

    Create html and css for rock paper scissor game

---
+++ HW7/RPS_Multiplayer/assets/javascript/game.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+
+    // Initialize Firebase
+    var config = {
+        apiKey: "AIzaSyCByOuDQrWL1lz3fvnbEDGGIwgcnGcq4w4",
+        authDomain: "rock-paper-scissor-f26a1.firebaseapp.com",
+        databaseURL: "https://rock-paper-scissor-f26a1.firebaseio.com",
+        projectId: "rock-paper-scissor-f26a1",
+        storageBucket: "",
+        messagingSenderId: "302275638088"
+    };
+    firebase.initializeApp(config);
+})
+
+
+
+
+
