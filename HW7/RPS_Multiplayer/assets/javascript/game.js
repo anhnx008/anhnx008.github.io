85c09018d51ab682b6e1f3c1170e6a2d8bf197b2
Anh <nguye538@umn.edu>
2018-06-23 10:23:03 -0500

    Added logic for game play

--- HW7/RPS_Multiplayer/assets/javascript/game.js
+++ HW7/RPS_Multiplayer/assets/javascript/game.js
@@ -45,6 +45,7 @@ $(document).ready(function () {
         $(".score").hide();
     }
 
+    //Display welcome message and let user knows which player they are
     function welcomePlayer() {
         if (whoamI === "player1") {
             $("#announcement-display").text("Hi " + player1.name + " ! " + "You are player 1.");
@@ -61,6 +62,36 @@ $(document).ready(function () {
             $("#playBtn").hide();
             $("#name-input").hide();
         }
+
+    }
+
+    function determineWinner(choice1, choice2){
+        
+        if ((choice1 === choice2) || (choice1 === choice2) || (choice1 === choice2)) 
+        {
+            //A draw
+            return "tie";
+        }
+        else if ((choice1 === "r" && choice2 === "s") || (choice1 === "p" && choice2 === "r") || (choice1 === "s" && choice2 === "p"))
+        {
+            //Player 1 wins
+            return "player1";
+        }
+        else if ((choice2 === "r" && choice1 === "s") || (choice2 === "p" && choice1 === "r") || (choice2 === "s" && choice1 === "p"))
+        {
+            //Player 2 wins
+            return "player2";
+        }
+    }
+
+    function resetTurn(){
+        playerTurn = 1;
+
+        $("#announcement-display").text("");
+
+        database.ref().update({
+            playerTurn: playerTurn
+        })
 
     }
     //*****End Helper Method Section**************
@@ -88,7 +119,7 @@ $(document).ready(function () {
             if (currentPlayer === "player1") {
                 player1.name = nameFromInputField.val().trim();
 
-                //Update firebase database with the new info
+                //Update firebase database with player 1's info
                 database.ref().update({
                     p1Name: player1.name,
                     p1Wins: player1.win,
@@ -101,7 +132,7 @@ $(document).ready(function () {
             else if (currentPlayer === "player2") {
                 player2.name = nameFromInputField.val().trim();
 
-                //Update firebase database with the new info
+                //Update firebase database with player 2's info
                 database.ref().update({
                     p2Name: player2.name,
                     p2Wins: player2.win,
@@ -112,7 +143,6 @@ $(document).ready(function () {
                 welcomePlayer();
             }
         }
-
     });
 
     //Ongoing - listening to database's changes
@@ -131,13 +161,13 @@ $(document).ready(function () {
             $("#p2Score").show();
             $("#player2Name").text(snapshot.val().p2Name);
             $("#win-count2").text(snapshot.val().p2Wins);
-            $("#loss-count2").text(snapshot.val().p2Losse);
+            $("#loss-count2").text(snapshot.val().p2Losses);
         }
 
         //If both players have entered their name then begin game
         if ((snapshot.val().p1Name !== undefined) && (snapshot.val().p2Name !== undefined)) {
             
-            //Player 1's turn
+            //And it is player 1's turn
             if (snapshot.val().playerTurn === 1) 
             {
                 if (whoamI === "player1") {
@@ -154,7 +184,7 @@ $(document).ready(function () {
                 }
             }
 
-            //Player 2's turn
+            //And it is player 2's turn
             else if (snapshot.val().playerTurn === 2) 
             {
                 if (whoamI === "player2") 
@@ -169,19 +199,59 @@ $(document).ready(function () {
                     $(".player2Btn").hide();
                 }
             }
+
+            else if(snapshot.val().playerTurn === 0)
+            {
+                //This means that both players have already picked
+                player1.rpsSelection = snapshot.val().p1choice;
+                player2.rpsSelection = snapshot.val().p2choice;
+
+                var winner = determineWinner(p1Choice, p2choice);
+
+                if(winner === "player1"){
+                    player1.win = snapshot.val().p1Wins;
+                    player1.win++;
+                    player2.loss = snapshot.val().p2Losses;
+                    player2.loss++;
+
+                    database.ref().update({
+                        p1Wins: player1.win,
+                        p2Losses: player2.loss,
+                    })
+                }
+
+                else if(winner === "player2")
+                {
+                    player2.win = snapshot.val().p2Wins;
+                    player2.win++;
+                    player1.loss = snapshot.val().p1Losses;
+                    player1.loss++;
+
+                    database.ref().update({
+                        p2Wins: player2.win,
+                        p1Losses: player1.loss,
+                    })
+                }
+                else if(winner === "tie")
+                {
+                    $("#announcement-display").text("It's a tie!");
+                }
+
+                setTimeout(resetTurn, 4000);
+            }
         }
 
         //If NO players defined in database then new user will be player 1
         if ((whoamI === "") && (snapshot.val().p1Name === undefined)) {
-            //Set the player attribute of the Play button to player 1
+            //Set the "player" attribute of the Play button to player 1
             $("#playBtn").attr("player", "player1")
         }
         // If player 1 exists the new user will be player 2
         else if (whoamI === "" && (snapshot.val().p2Name === undefined)) {
-            //Set the player attribute of the Play button to player2
+            //Set the "player" attribute of the Play button to player2
             $("#playBtn").attr("player", "player2")
         }
-        //If both spots are taken then anyone joining a spectators
+        //If both spots are taken then anyone joining is a spectator
         else if (whoamI === "") {
             $("#announcement-display").text("There is currently no available spot. But stay and watch!");
         }
@@ -189,6 +259,48 @@ $(document).ready(function () {
     }, function(errorObject) {
         console.log("Error handled " + errorObject.code);
     });
+
+    //Listen to the click event for the 3 options from player 1
+    $(document).on("click", ".player1Btn", function(){
+
+        var selection = $(this).attr("data-value");
+
+        if(playerTurn === 1)
+        {
+            playerTurn = 2;
+
+            //Push to database
+            database.ref().update({
+                p1choice : selection,
+                playerTurn: playerTurn
+            });
+
+            //After player 1 has made the selection then hide the rsp options
+            $(".playerBtn1").hide();
+        }
+    });
+
+    $(document).on("click", ".player2Btn", function(){
+
+        var selection = $(this).attr("data-value");
+
+        if(playerTurn === 2)
+        {
+            playerTurn = 0; //This stand for both player have already picked their choices
+
+            //Push to database
+            database.ref().update({
+                p2choice : selection,
+                playerTurn: playerTurn
+            });
+
+            //After player 2 has made the selection then hide the rsp options
+            $(".playerBtn2").hide();
+        }
+    });
+
+
+    })
 
     //Listening to SEND CHAT button's click event
     $("#sendBtn").on("click", function (event) {
